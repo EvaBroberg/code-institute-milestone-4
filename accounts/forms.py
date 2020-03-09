e8fbd029d78f341e44ca75645b495071dd84a627
ieva broberg <ievabroberg@gmail.com>
2020-03-09 15:22:26 +0000

    added registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,21 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
-class UserLoginForm(forms.Form):
+class LoginForm(forms.Form):
     """User login form"""
-    
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+    
+
+class RegistrationForm(UserCreationForm):
+    """User registration form"""
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password',widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
+    
+    
--- accounts/views.py
+++ accounts/views.py
@@ -3,13 +3,15 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect, reverse
 from django.contrib import auth
-from accounts.forms import UserLoginForm
+from django.contrib.auth.decorators import login_required
+from accounts.forms import LoginForm, RegistrationForm
 
 
 def index(request):
     """Return index page"""
     return render(request, 'index.html')
 
+@login_required
 def logout(request):
     """Logout user"""
     auth.logout(request)
@@ -17,16 +19,24 @@ def logout(request):
 
 def login(request):
     """Return login page"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == 'POST':
-        login_form = UserLoginForm(request.POST)
+        login_form = LoginForm(request.POST)
         
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
             
             if user:
                 auth.login(user=user, request=request)
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, 'The Email Address or Password you entered is incorrect.')
     else:
-        login_form = UserLoginForm()
+        login_form = LoginForm()
     return render(request, 'login.html', {'login_form': login_form})
+
+def register(request):
+    """Return registration page"""
+    registration_form = RegistrationForm()
+    return render(request, 'register.html', {'registration_form': registration_form}) 
