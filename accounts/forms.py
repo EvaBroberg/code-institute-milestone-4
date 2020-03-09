700ca267477660ddac2f2208a07018f3cbd264d5
ieva broberg <ievabroberg@gmail.com>
2020-03-09 15:42:41 +0000

    added validation for registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,11 +11,31 @@ class LoginForm(forms.Form):
 
 class RegistrationForm(UserCreationForm):
     """User registration form"""
-    password1 = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(label='Password',widget=forms.PasswordInput)
     password2 = forms.CharField(label='Confirm password',widget=forms.PasswordInput)
     
     class Meta:
         model = User
         fields = ['email', 'username', 'password1', 'password2']
+        
+
+def clean_email(self):
+    email = self.cleaned_data.get('email')
+    username = self.cleaned_data.get('username')
+    if User.objects.filter(email=email).exclude(username=username):
+        raise forms.ValidationError(u'This email is already registered.')
+    return email
+
+
+def clean_password(self):
+    password1 = self.cleaned_data.get('password1')
+    password2 = self.cleaned_data.get('password2')
+    if not password1 or not password2:
+        raise forms.ValidationError(u'Please confirm your password.')
+    if password1 !== password2:
+        raise forms.ValidationError(u'Passwords must match.')
+    return password2
+
+
     
     
