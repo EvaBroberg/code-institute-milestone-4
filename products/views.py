92baf115ca1b5997cb28fa5c8918022dd86bb1ca
ieva broberg <ievabroberg@gmail.com>
2020-03-11 17:11:21 +0000

    added products views and urls

--- products/views.py
+++ products/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def all_products(request):
+    products = Products.objects.all()
+    return render(request, "products.html", {"products": products})
