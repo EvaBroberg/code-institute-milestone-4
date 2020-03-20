634737edd09b62de0013284ca38723bbeb9415a5
ieva broberg <ievabroberg@gmail.com>
2020-03-20 18:59:55 +0000

    added delete and drafts views and urls

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from blog.models import Post, Comment
 from blog.forms import PostForm,CommentForm
+from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
 
 
 class PostListView(ListView):
     model = Post
-    template_name = "TEMPLATE_NAME"
     
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     
 class PostDetailView(DetailView):
     model = Post
@@ -28,5 +28,20 @@ class PostUpdateView(LoginRequiredMixin,UpdateView):
     redirect_field_name = 'blog/post_detail.html'
     form_class = PostForm
     model = Post
+    
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+    
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '../accounts/templates/login.html'
+    redirect_field_name = 'blog/post_list.html'
+     model = Post
+     
+     def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+    
+    
+
     
     
