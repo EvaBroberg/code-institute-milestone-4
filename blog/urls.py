55ac49e7be29da2a50c2025f6489dd2a4804e9a3
ieva broberg <ievabroberg@gmail.com>
2020-04-28 12:33:34 +0100

    debuggeed blog app, because urls were not linking correctly

--- blog/urls.py
+++ blog/urls.py
@@ -4,10 +4,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.PostListView.as_view(template_name='post_list.html'), name='post_list'),
     url(r'^community/',views.CommunityView.as_view(),name='community'),
-    url(r'^post/(?P<pk>\d+)$',views.PostDetailView.as_view(),name='post_detail'),
+    url(r'^post/(?P<pk>\d+)$',views.PostDetailView.as_view(template_name='post_detail.html'),name='post_detail'),
     url(r'^post/new/$',views.CreatePostView.as_view(),name='post_new'),
-    url(r'^post/(?P<pk>\d+)/edit/$',views.PostUpdateView.as_view(),name='post_edit'),
-    url(r'^post/(?P<pk>\d+)/remove/$',views.PostDeleteView.as_view(),name='post_remove'),
+    url(r'^post/(?P<pk>\d+)/edit/$',views.PostUpdateView.as_view(template_name='post_form.html'),name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/remove/$',views.PostDeleteView.as_view(template_name='post_confirm_delete.html'),name='post_remove'),
     url(r'^drafts/$',views.DraftListView.as_view(),name='post_draft_list'),
     url(r'^post/(?P<pk>\d+)/comment/$',views.add_comment_to_post, name='add_comment_to_post'),
     url(r'^comment/(?P<pk>\d+)/approve/$',views.comment_approve, name='comment_approve'),
