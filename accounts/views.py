83154b67deefd34c6d9908cbd58e264b64403337
ieva broberg <ievabroberg@gmail.com>
2020-04-30 00:12:34 +0100

    added form authentication for updating profile and success message

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from accounts.forms import LoginForm, RegistrationForm, ContactForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
 
 
 def index(request):
@@ -20,9 +21,6 @@ def index(request):
     else:
         contact_form = ContactForm()
     return render(request, 'index.html',{'contact_form':contact_form}) 
-
-
-
 
 
 @login_required
@@ -82,7 +80,21 @@ def register(request):
 @login_required
 def profile(request):
     """User profile page"""
-    user_update_form = UserUpdateForm()
-    profile_update_form = ProfileUpdateForm()
     user = User.objects.get(email=request.user.email)
+    
+    if request.method == 'POST':
+        user_update_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_update_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        
+        if user_update_form.is_valid() and profile_update_form.is_valid():
+            user_update_form.save()
+            profile_update_form.save()
+            messages.success(request, f'Your profile had been updated!')
+            return redirect('profile')
+        
+    else:
+        user_update_form = UserUpdateForm(instance=request.user)
+        profile_update_form = ProfileUpdateForm(instance=request.user.profile)
+        
+        
     return render(request, 'profile.html', {'profile':user, 'user_update_form':user_update_form, 'profile_update_form':profile_update_form})
