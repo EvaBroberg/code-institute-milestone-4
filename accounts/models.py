a7fb34aab7c7758bd3609554db1754a5c1947547
ieva broberg <ievabroberg@gmail.com>
2020-04-30 20:37:34 +0100

    profile page styling, adding extra features for paying users

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -18,3 +19,14 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
     
+    def save(self):
+        super().save()
+        
+        img = Image.open(self.image.path)
+        
+        if img.height > 200 or img.width > 200:
+            output_size = (200, 200)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+        
+        
