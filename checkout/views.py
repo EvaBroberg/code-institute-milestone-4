885115b6f8be5f31575585271a5798b7922ae574
ieva broberg <ievabroberg@gmail.com>
2020-05-26 10:41:03 +0100

    added contextprocessor file to checkout, amended views to process stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,89 +1,115 @@
-from django.shortcuts import render, get_object_or_404, redirect, reverse
-
-from django.http import HttpResponse, JsonResponse
-
-from django.contrib.auth.forms import UserCreationForm
-
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import Group
-from django.contrib import auth
-
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-
-from .forms import OrderForm, PaymentForm
-
-from django.conf import settings
-from django.utils import timezone
-
-from .models import Order, OrderLineItem
+from django.shortcuts import render, redirect
+from cart.models import Cart, CartItem
 from products.models import Product
-from accounts.models import Customer
+from django.core.exceptions import ObjectDoesNotExist
+from orders.models import Order, OrderItem
+from cart.views import __cart_id
 
 import stripe
+from django.conf import settings
 
-# Create your views here.
 
-stripe.api_key = settings.STRIPE_SECRET_KEY
 
-@login_required()
-def checkout(request):
-    if request.method == 'POST':
-        order_form = OrderForm(request.POST)
-        payment_form = PaymentForm(request.POST)
+def checkout(request, total=0, counter=0, cart_items=None):
+    """
+    retrieve all cart items,
+    calculate total cost of all products in the cart
+    """
+    try:
+        cart       = Cart.objects.get(cart_id=__cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, active=True)
         
-        if order_form.is_valid() and payment_form.is_valid():
-            order = order_form.save(commit=False)
-            order.date = timezone.now()
-            order.save()
+        for cart_item in cart_items:
+            total   += (cart_item.product.price * cart_item.quantity)
+            counter += cart_item.quantity
             
-            cart = request.session.get('cart', {})
-            total  = 0
-            for id, quantity in cart.items():
-                product = get_object_or_404(Product, pk=id)
-                total += quantity * product.price
-                order_line_item = OrderLineItem(order = order, product = product, quantity = quantity)
-                order_line_item.save()
-                
-            try:
-                token = self.request.POST.get('stripeToken')
-                customer = stripe.Customer.create()
-                stripe.Charge.create(
-                    amount=int(total * 100),
-                    currency="EUR",
-                    description=request.user.email,
-                    card=payment_form.cleaned_data['stripe_id']
-                )
-            except stripe.error.CardError:
-                messages.error(request, "Your card was declined")
-                
-            if customer.paid:
-                for id, quantity in cart.items():
-                    product = get_object_or_404(Product, pk=id)
-                    product.stock_count -= quantity
-                    product.save()
+    except ObjectDoesNotExist:
+        pass
 
-                request.session['cart'] = {}
-                messages.error(request, "You have successfully paid")
-                return redirect(reverse('products'))
-            else:
-                messages.error(request, "Unable to take payment")
-        else:
-            print(payment_form.errors)
-            messages.error(
-                request, "We were unable to take a payment with that card!")
-    else:
-        payment_form = PaymentForm()
-        order_form = OrderForm()
-        
-    return render(
-        request, "checkout.html", {
-            "order_form": order_form,
-            "payment_form": payment_form,
-            "publishable": settings.STRIPE_PUBLISHABLE_KEY
-        }
-    )
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    stripe_total   = int(total * 100) 
+    description    = 'uGoGirl -New Order'
+    data_key       = settings.STRIPE_PUBLISHABLE_KEY
     
-
+    if request.method == 'POST':
+        
+        try:
+            token           = request.POST['stripeToken']
+            email           = request.POST['stripeEmail']
+            
+            billingName     = request.POST['stripeBillingName']
+            billingAddress1 = request.POST['stripeBillingAddressLine1']
+            billingCity     = request.POST['stripeBillingAddressCity']
+            billingPostCode = request.POST['stripeBillingAddressZip']
+            billingCountry  = request.POST['stripeBillingAddressCountryCode']
+            
+            shippingName     = request.POST['stripeShippingName']
+            shippingAddress1 = request.POST['stripeShippingAddressLine1']
+            shippingCity     = request.POST['stripeShippingAddressCity']
+            shippingPostCode = request.POST['stripeShippingAddressZip']
+            shippingCountry  = request.POST['stripeShippingAddressCountryCode']
+            
+            
+            customer = stripe.Customer.create(
+                email=email,
+                source=token
+                )
+            
+            charge = stripe.Charge.create(
+                amount=stripe_total,
+                currency='gbp',
+                description=description,
+                customer=customer.id
+                )
+            
+            # create the order
+            try:
+                order_details = Order.objects.create(
+                    token           = token,
+                    total           = total,
+                    email           = email,
+                    
+                    billingName     = billingName,
+                    billingAddress1 = billingAddress1,
+                    billingCity     = billingCity,
+                    billingPostCode = billingPostCode,
+                    billingCountry  = billingCountry,
+                    
+                    shippingName     = shippingName,
+                    shippingAddress1 = shippingAddress1,
+                    shippingCity     = shippingCity,
+                    shippingPostCode = shippingPostCode,
+                    shippingCountry  = shippingCountry
+                    
+                )
+                
+                order_details.save()
+                
+                for order_item in cart_items:
+                    or_item = OrderItem.objects.create(
+                        product  = order_item.product.name,
+                        quantity = order_item.quantity,
+                        price    = order_item.product.price,
+                        order    = order_details 
+                    )
+                    
+                    or_item.save()
+                    
+                    # reduce stock
+                    products       = Product.objects.get(id=order_item.product.id)
+                    products.stock = int(order_item.product.stock - order_item.quantity)
+                    
+                    products.save()
+                    order_item.delete()
+                
+                return redirect('thank_you_page', order_details.id)
+            
+            except ObjectDoesNotExist:
+                pass
                 
             
+            
+        except stripe.error.CardError as e:
+            return False, e
+    
+    return render(request, 'cart.html', dict(cart_items=cart_items, total=total, counter=counter, data_key=data_key, stripe_total=stripe_total,description=description))
