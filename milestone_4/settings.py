b10b4021b3b714ebde19b1e6d9246f3fe25a1a0d
ieva broberg <ievabroberg@gmail.com>
2020-05-28 21:15:00 +0100

    amended requirements file

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -114,7 +114,7 @@ WSGI_APPLICATION = 'milestone_4.wsgi.application'
 # DATABASES = { 'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 
 
-DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+DATABASES = {"default": dj_database_url.parse(os.environ.get(DATABASE_URL))}
 
 
 
