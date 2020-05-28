c4576e22606dc4acf4ada04ececba8f5cdac9c98
ieva broberg <ievabroberg@gmail.com>
2020-05-28 21:09:49 +0100

    amended requirements file

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -11,8 +11,13 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
 import dj_database_url
+
+if os.path.exists('env.py'):
+    import env
+
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR   = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -108,15 +113,8 @@ WSGI_APPLICATION = 'milestone_4.wsgi.application'
 
 # DATABASES = { 'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 
-if development:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
-else:
-    DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+
+DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 
 
