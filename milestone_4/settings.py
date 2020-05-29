04b1746399f0d9b083cecacec2dff3c8982e767f
ieva broberg <ievabroberg@gmail.com>
2020-05-29 13:59:20 +0100

    changed database settings

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -116,12 +116,23 @@ WSGI_APPLICATION = 'milestone_4.wsgi.application'
 
 # DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     }
+# }
 
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
 
 
 
