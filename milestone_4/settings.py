c64060528c6c70ea64158fcc781eb72a0cd4fe28
ieva broberg <ievabroberg@gmail.com>
2020-05-31 23:51:38 +0100

    added runtime.txt

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['milestone-4-ugogirl.herokuapp.com']
+ALLOWED_HOSTS = ['milestone-4-ugogirl.herokuapp.com', ' 127.0.0.1']
 
 
 # Application definition
