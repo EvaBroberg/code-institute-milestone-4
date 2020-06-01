12912b893e53efbcae64d05784b3a1311ae4a580
ieva broberg <ievabroberg@gmail.com>
2020-06-01 01:10:05 +0100

    amended procfile

--- milestone_4/settings.py
+++ milestone_4/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+import dj_database_url
 import os
 import django_heroku
 
@@ -113,9 +114,7 @@ else:
         }
     }
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASE['default'].update(db_from_env)
+
 
 
 # Password validation
